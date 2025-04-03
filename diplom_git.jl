0d9aabfedba1875f4d837935504d8d48bd42f252
Diniska1 <144814416+Diniska1@users.noreply.github.com>
2025-04-03 05:20:16 +0300

    Add files via upload

--- diplom_git.jl
+++ diplom_git.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra
+using FileIO, JLD2
 
-eps = 1e-5
+eps = 1e-3
 
 function A_scal(A, a, b)
     return (A * a)' * b
@@ -11,10 +12,10 @@ function print_matrix(A, msg="")
     display(A)
 end
 
-function qr_gram_schmidt(A::Matrix{Float64}, B, inner_product::Function)
+function qr_gram_schmidt(A, B, inner_product::Function)
     m, n = size(A)
-    Q = zeros(Float64, m, n)
-    R = zeros(Float64, n, n)
+    Q = zeros(ComplexF64, m, n)
+    R = zeros(ComplexF64, n, n)
 
     for j = 1:n
         v = A[:, j]  
@@ -24,7 +25,7 @@ function qr_gram_schmidt(A::Matrix{Float64}, B, inner_product::Function)
             v = v - R[i, j] * Q[:, i]
         end
 
-        R[j, j] = sqrt(inner_product(B, v, v))
+        R[j, j] = sqrt(real(inner_product(B, v, v)))
         Q[:, j] = v / R[j, j]
     end
 
@@ -65,6 +66,16 @@ function check_orth(A, P, P_prev)
     return res
 end
 
+function norms2(R)
+    norms = Float64[]
+
+    for i in size(R, 2)
+        push!(norms, sqrt(R[:, i]' * R[:, i]))
+    end    
+
+    return norms
+end
+
 function traspose_perm_matrix(a)
     n = size(a, 1)
     b = zeros(Int, n)
@@ -74,6 +85,14 @@ function traspose_perm_matrix(a)
     return b
 end
 
+function scale_columns(A, b)
+    """
+    res is matrix A: A[:, i] = A[:, i] * b[i] 
+    """
+    A = A .* reshape(b, (1, length(b)))
+    return A
+end
+
 function BCG(A, B)
     n = size(A, 1)
     n1, m = size(B) # amount right sides
@@ -81,74 +100,101 @@ function BCG(A, B)
         println("DimensionMismatch, rows A != rows B")
         exit()
     end
-    r = 3          # block size
+    r = 10          # block size
+
+    norms2_B = Float64[]
+    norms2_B_div = Float64[]
+    for i in 1:size(B, 2)
+        t = sqrt(B[:, i]' * B[:, i])
+        push!(norms2_B, t)
+        push!(norms2_B_div, 1 / t)
+    end    
+
+    B = scale_columns(B, norms2_B_div)
 
     X = zeros(n, m)     # starting value
     R = B - A * X               # n x m
 
     F = qr(R, Val(true)) 
     S = copy(F.p[1:r])
+    sort!(S)
     P = copy(R[:, S])                   # n x r
     P_prev = copy(P)
 
-    used_columns = Int32[]
+    converged_columns = Int32[]
     c_norms = Float64[]
-
     V = Matrix{Float64}(undef, n, 0)
 
     iteration = 1
-    maxiter = 1000
+    maxiter = 2000
     while iteration < maxiter
 
-        if iteration > 2 && iteration % 2 == 0
-            push!(c_norms, check_orth(A, P, P_prev))
-            P_prev = P
+        if iteration % 25 == 0
+            println("iteration = $iteration")
+            println("cnorm B - AX: ", maximum(abs.(R)))
+            println("max(2norm(R[i])): ", maximum(norms2(R)))
         end
-
-        # if iteration % 100 == 0 && iteration > 2
-        #     println("iter = $iteration, max c_norms is ",maximum(c_norms))
-        # end    
-
         
-        if isapprox(det(P' * A * P), 0.0)
-            print_matrix(P' * A * P, "P' * A * P")
-            println("P'AP SINGULAR")
-            exit()
-        end
-        alpha = (P' * A * P) \ P' * R       # r x m
-        X = X + P * alpha                   # n x m
-        R = R - A * P * alpha               # n x m
+        AP = A * P;
+        PAP = P' * AP;
+        PAP = (PAP + PAP') / 2;
+        L = cholesky(PAP).L;
 
-        F = qr(R[:,S], Val(true))                   # PQR
+        alpha = L' \ (L \ (P' * R))       # r x m
+        X = X + P * alpha                   # n x m
+        R = R - AP * alpha               # n x m
+            
+        normsR = norms2(R)
+        if (maximum(normsR)) < eps
+            break
+        end
+
+        _, TMP = qr(R[:, S])
+        F = qr(TMP, Val(true))                   # PQR
         M = F.p
         T = F.R
 
         ind = check(T)                              # find index small elem on diag(T)
         if ind == size(T, 1) + 1                    # if all elements on diag(T) is large
-            beta = (P' * A * P) \ P' * A * R[:,S]
-            P = R[:, S] - P * beta
+            beta = L' \ (L \ (AP' * R[:,S]))
+            Pn = R[:, S] - P * beta
 
-            # if isempty(V) == false
-            #     gamma = V' * A * R[:, S]
-            #     P = P - V * gamma
-            # end
+            if isempty(V) == false
+                AV = A * V
+                gamma = (AV' * V) \ (AV' * R[:, S])
+                Pn = Pn - V * gamma
+            end
+
+            beta = L' \ (L \ (AP' * Pn))
+            Pn -= P * beta
+
+            if isempty(V) == false
+                AV = A * V
+                gamma = (AV' * V) \ (AV' * Pn)
+                Pn -= V * gamma
+            end
+            P = Pn;
 
         else
+            print("\nCONVERGE: (iter = $iteration)")
 
             add_used_cols = [ S[F.p[i]] for i in ind:size(F.p, 1) ]
-            used_columns = [ used_columns ; add_used_cols ]                      # indexes of columns that converged
-            others_cols = setdiff( 1:m, [ used_columns ; S[ F.p[1:ind-1] ] ] )   # indexes of cols that in R and not in used_columns and not in current block
+            converged_columns = [ converged_columns ; add_used_cols ]                      # indexes of columns that converged
+            others_cols = setdiff( 1:m, [ converged_columns ; S[ F.p[1:ind-1] ] ] )         # indexes of cols that in R and not in converged_columns and not in current block
             
             if isempty(others_cols) == true && isempty(F.p[1:ind-1]) == true                                # stopping criteria
-                println("!!! others_cols is empty !!!")                     # if all indexes of cols in used_columns than all converged than break 
+                println("!!! others_cols is empty !!!")                     # if all indexes of cols in converged_columns than all converged than break 
                 break
             end
 
             take_cols = min(size(T, 1) - ind + 1, size(others_cols, 1))     # amount columns to take in block
+            print(" take_cols = $take_cols ")
 
-            F = qr(R[:, others_cols], Val(true))                            # PQR
+            _, TMP = qr(R[:, others_cols])
+            F = qr(TMP, Val(true))                            # PQR
             S_cap = others_cols[ F.p[1:take_cols] ]
             S = [ S[M[1:ind-1]] ; S_cap]
+            sort!(S)
             
             M_transposed = traspose_perm_matrix(M)
             Q_cap, R_cap = qr_gram_schmidt(P[:, M_transposed], A, A_scal) # QR decomp with A-scalar product
@@ -156,11 +202,24 @@ function BCG(A, B)
             P_cap = Q_cap[:, 1:take_cols]
             P_wave = Q_cap[:, (take_cols+1):size(Q_cap, 2)]
 
+            nu = ((P_cap' * A) * P_cap) \ ((P_cap' * A) * P_wave);
+            P_wave -= P_cap * nu;
+
             V = hcat(V, P_wave)                                          # column concatenation
 
-            beta = P_cap' * A * R[:, S]
-            gamma = V' * A * R[:, S]
+            AV = A * V
+            VAV = V' * AV
+
+            beta = ((P_cap' * A) * P_cap) \ ((P_cap' * A) * R[:, S])
+            gamma = (VAV) \ (AV' * R[:, S])
             P = R[:, S] - P_cap * beta - V * gamma
+
+            beta = ((P_cap' * A) * P_cap) \ ((P_cap' * A) * P)
+            gamma = (VAV) \ (AV' * P)
+            P -= P_cap * beta + V * gamma
+
+            
+            println("end")
         end
         
         iteration += 1
@@ -171,30 +230,23 @@ function BCG(A, B)
     if isempty(c_norms) == false
         println("max(c_norms of P_{k+2}' A P_k): ", maximum(c_norms))
     end
+    X = scale_columns(X, norms2_B)
+    
     return X, iteration
 end
 
-n = 200
-A = float(zeros(n,n))
-for i in 1:n
-    if i != 1
-        A[i - 1, i] = i - 1
-        A[i, i - 1] = i - 1
-    end
-    if i > 3
-        A[i - 3, i] = 1
-        A[i, i - 3] = 1
-    end
-    A[i, i] = 2 * i + 1
-end
-if isposdef(A) == false
-    println("Matrix A is NOT positive definitive or not symmertic\n")
-    exit()
-end
-B = float(hcat(collect(1:n), circshift(collect(1:n), 1),  circshift(collect(1:n), 2), circshift(collect(1:n), 3), circshift(collect(1:n), 4)))
 
+f = FileIO.load("matrix.jld2");
+A = f["A"];
+B = f["B"];
+
+println("n = ", size(A, 1))
+println("right sides = ", size(B, 2))
+
+println("function started")
 
 X, iter = BCG(A, B)
 
 println("iterations = $iter")
 println("C_norm of B - AX is ", maximum(abs.(B - A*X)))
+println("max(2norm(B - AX)) is ", maximum(norms2(B - A*X)))
